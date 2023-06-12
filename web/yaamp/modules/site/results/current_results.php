2eaec2527758bfa8e98d7dab3c6b8e0ce8ac4671
msy2008 <963858700@qq.com>
2023-06-12 11:35:31 +0800

    Fix the problem that the pool status disappears after using merged mining

--- web/yaamp/modules/site/results/current_results.php
+++ web/yaamp/modules/site/results/current_results.php
@@ -145,6 +145,47 @@ foreach($algos as $item)
 
 	echo "</tr>";
 
+        if ($coins > 0){
+        $list = getdbolist('db_coins', "enable and visible and auto_ready and algo=:algo order by index_avg desc", array(':algo'=>$algo));
+        foreach($list as $coin){
+        $name = substr($coin->name, 0, 12);
+        $symbol = $coin->getOfficialSymbol();
+        echo "<tr>";
+        echo "<td align='left' valign='top' style='font-size: .8em;'><img width='16' src='".$coin->image."'>  <b>$name</b> <span style='font-size: .8em'></span></td>";
+        $port_count = getdbocount('db_stratums', "algo=:algo and symbol=:symbol", array(':algo'=>$algo,':symbol'=>$symbol));
+        $port_db = getdbosql('db_stratums', "algo=:algo and symbol=:symbol", array(':algo'=>$algo,':symbol'=>$symbol));
+
+        if($port_count == 1)
+            echo "<td align='right' style='font-size: .8em;'>.$port_db->port.</td>";
+        else
+            echo "<td align='right' style='font-size: .8em;'>$port</td>";
+
+            echo "<td align='right' style='font-size: .8em;'>$symbol</td>";
+
+        if($port_count == 1)
+            echo "<td align='right' style='font-size: .8em;'>.$port_db->workers.</td>";
+        else
+            echo "<td align='right' style='font-size: .8em;'>$workers</td>";
+
+        $pool_hash = yaamp_coin_rate($coin->id);
+        $pool_hash_sfx = $pool_hash? Itoa2($pool_hash).'h/s': '';
+
+        $pool_hash_pow = yaamp_pool_rate_pow($coin->algo);
+        $pool_hash_pow_sfx = $pool_hash_pow? Itoa2($pool_hash_pow).'h/s': '';
+
+        if($coin->auxpow && $coin->auto_ready)
+            echo "<td align='right' style='font-size: .8em; opacity: 0.6;'>$pool_hash_pow_sfx</td>";          
+        else
+            echo "<td align='right' style='font-size: .8em;'>$pool_hash_sfx</td>";
+
+            echo "<td align='right' style='font-size: .8em;'>{$fees}%</td>";
+        $btcmhd = yaamp_profitability($coin);
+        $btcmhd = mbitcoinvaluetoa($btcmhd);
+        echo "<td align='right' style='font-size: .8em;'>$btcmhd</td>";
+        echo "</tr>";
+    }
+} 
+	
 	$total_coins += $coins;
 	$total_miners += $workers;
 }
