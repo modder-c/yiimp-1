5d4794f30b65563f643a6619c7487dc0ea14d8b6
msy2008 <963858700@qq.com>
2024-10-21 10:38:11 +0800

    The same block earning is added twice. ..The code is being tested. . .

--- web/yaamp/core/backend/clear.php
+++ web/yaamp/core/backend/clear.php
@@ -1,52 +1,51 @@
-<?php
-
-function BackendClearEarnings($coinid = NULL)
-{
-//	debuglog(__FUNCTION__);
-
-	$delay = time() - (int) YAAMP_CLEARS_DELAY; 
-
-	$total_cleared = 0.0;
-
-	$sqlFilter = $coinid ? " AND coinid=".intval($coinid) : '';
-
-	$list = getdbolist('db_earnings', "status=1 AND mature_time<$delay $sqlFilter");
-	foreach($list as $earning)
-	{
-		$coin = getdbo('db_coins', $earning->coinid);
-		if(!$coin)
-		{
-		    $earning->delete();
-		    continue;
-		}
-
-		if ($coin->symbol === 'DOGM') {  
-			$user = getdbo('db_accountsdogm', $earning->userid);  
-		} elseif ($coin->symbol === 'DOGE') {  
-			$user = getdbo('db_accountsdoge', $earning->userid);  
-		} else {  
-			$user = getdbo('db_accounts', $earning->userid);  
-		}  
-
-		if(!$user)  
+<?php  
+  
+function BackendClearEarnings($coinid = NULL)  
+{  
+	$delay = time() - (int) YAAMP_CLEARS_DELAY;   
+  
+	$total_cleared = 0.0;  
+  
+	$sqlFilter = $coinid ? " AND coinid=".intval($coinid) : '';  
+  
+	$list = getdbolist('db_earnings', "status=1 AND mature_time<$delay $sqlFilter");  
+	foreach($list as $earning)  
+	{  
+		$coin = getdbo('db_coins', $earning->coinid);  
+		if(!$coin)  
 		{  
-			$earning->delete();  
-			continue;  
+		    $earning->delete();  
+		    continue;  
 		}  
   
-		$earning->status = 2;		// cleared  
-		$earning->save();  
-                
-                $value = $earning->amount; 
+		if ($coin->symbol === 'DOGM') {    
+			$user = getdbo('db_accountsdogm', $earning->userid);    
+		} elseif ($coin->symbol === 'DOGE') {    
+			$user = getdbo('db_accountsdoge', $earning->userid);    
+		} else {    
+			$user = getdbo('db_accounts', $earning->userid);    
+		}    
   
-		$user->balance += $value;  
-		$user->save();  
-  
-		if($user->coinid == 6)  
-		$total_cleared += $value;  
-	}  
-  
-	if($total_cleared>0)  
-		debuglog("total cleared from mining $total_cleared BTC");  
+		if(!$user)    
+		{    
+			$earning->delete();    
+			continue;    
+		}    
+    
+		$earning->status = 2; 
+		$earning->save();    
+                  
+                $value = $earning->amount;   
+    
+		$user->balance += $value;    
+		$user->save();    
+    
+		$total_cleared += $value; 
+	}    
+    
+	if($total_cleared > 0)    
+	{    
+		debuglog("Total cleared earnings: " . $total_cleared);    
+	}    
 }
 
